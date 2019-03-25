eb68da3b6150a57cf8220e8a396ab9bdef21f0d0
jianfeng.wang <jianfeng.wang@maixunbytes.com>
2019-03-25 16:31:34 +0800

    add C++ config

---
+++ play_jieba/viterbi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+#include <map>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello" << endl;
+    vector<int> a;
+    a.push_back(5);
+    cout << a[0] << endl;
+    system("pause");
+    return 0;
+}
